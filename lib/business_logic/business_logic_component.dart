7a18ed0f8e2a3ceda427e77232d4b21e90c64ee8
Zbigniew Gorawski <z.gorawski@nomtek.com>
2023-06-13 11:22:42 +0200

    improved test task

---
+++ lib/business_logic/business_logic_component.dart
@@ -0,0 +1,6 @@
+class BusinessLogicComponent {
+  /// Returns the total character count from words starting with the letter 'A' in the list.
+  int countingOperation(List<String?> wordsList) {
+    throw UnimplementedError();
+  }
+}
--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,17 +1,16 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:interview/business_logic/business_logic_component.dart';
 
-class BusinessLogicComponent {
-  // Returns the total character count from words starting with the letter 'A' in the list.
-  int businessOperation(List<String?> wordsList) {
-    throw UnimplementedError();
-  }
-}
+/// Interview task #1
+///
+/// 1. Examine what `countingOperation` does and what should be expected result for the given test data
+/// 2. Write a `countingOperation` body so the test passes
 
 void main() {
   test('BusinessLogicComponent test', () {
     final testData = ['Apple', null, 'Banana', 'Ananas', 'Peach', '🍒'];
     final sut = BusinessLogicComponent();
 
-    expect(sut.businessOperation(testData), -1);
+    expect(sut.countingOperation(testData), ??);
   });
 }
